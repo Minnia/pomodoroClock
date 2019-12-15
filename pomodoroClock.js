ca176be3d428806f8ba5da7a02f1744f3bb9fc1a
Minnia <jasminestockholm@gmail.com>
2019-12-15 23:15:31 +0100

    first commit

---
+++ pomodoroClock.js
@@ -0,0 +1,31 @@
+const MS_TO_MINUTE = 60000;
+const getEndDate = minutes => new Date().getTime() + MS_TO_MINUTE * minutes;
+const endDate = getEndDate(25);
+let now = new Date();
+let totalTimeRemaining = endDate - now;
+let minutesRemaining = Math.floor(
+  (totalTimeRemaining % (1000 * 60 * 60)) / (1000 * 60)
+);
+let secondsRemaining = Math.floor((totalTimeRemaining % (1000 * 60)) / 1000);
+const timer = setInterval(() => {
+  if (secondsRemaining === 0) {
+    minutesRemaining--;
+    secondsRemaining = 60;
+  }
+
+  if (totalTimeRemaining >= 0) {
+    minutesRemaining;
+    secondsRemaining--;
+    document.getElementById("timer-mins").innerHTML =
+      ("0" + minutesRemaining).slice(-2) + "<span class='label'>MIN(S)</span>";
+
+    document.getElementById("timer-secs").innerHTML =
+      ("0" + secondsRemaining).slice(-2) + "<span class='label'>SEC(S)</span>";
+  } else {
+    document.getElementById("timer").innerHTML = "The countdown is over!";
+  }
+}, 1000);
+
+function addOneMinute() {
+  minutesRemaining += 1;
+}
